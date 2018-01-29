9b178122ef414676036d9470ded656eab9eb609c
tmhage <attmhage@gmail.com>
2018-01-29 17:49:04 +0100

    first commit

---
+++ containers/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+export default class App extends Component {
+    render () {
+        return <Button bsStyle="primary">Click Me!</Button>
+    }
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+
+// main App
+import App from './containers/App';
+
+ReactDOM.render(<App />, document.getElementById('app'))
